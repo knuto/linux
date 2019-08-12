9e2d48351bad630e24a0f358724f3383ef8f231e
Knut Omang <knut.omang@oracle.com>
2019-08-12 08:11:07 +0200

    ktf: Integration logic for running ktf tests from googletest
    Currently ktf only supports integration with googletest on the user
    side, but there's nothing that prevents integration towards other user
    land frameworks for running and reporting, if so desired.
    
    Signed-off-by: Knut Omang <knut.omang@oracle.com>

---
+++ tools/testing/selftests/ktf/lib/ktf_unlproto.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
+ *    Author: Knut Omang <knut.omang@oracle.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * unlproto.c: This file is needed because the C struct init
+ * used in kernel/unlproto.h is not allowed in C++
+ */
+
+#include <netlink/netlink.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+#define NL_INTERNAL 1
+#include "kernel/ktf_unlproto.h"
+
+
+struct nla_policy *ktf_get_gnl_policy(void)
+{
+  return ktf_gnl_policy;
+}
